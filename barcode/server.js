9841a9b863b5ee6379f04d1bf25a12257d1412cf
Khanh Dao <kdao@Khanhs-MBP.attlocal.net>
2016-02-27 15:38:18 -0800

    Initial check in for qrcode rendering

--- barcode/server.js
+++ barcode/server.js
@@ -14,11 +14,22 @@ app.get('/', function(req, res) {
 });
 
 app.get('/imagerender', function(req, res) {
-  var code = qr.svgObject('San Joser Performance Art Center', { type: 'svg', size: 200 });
+  var code = qr.svgObject('San Joser Performance Art Center', { type: 'svg', size: 5 });
   //console.log(code);
   res.render('index', {path: code.path});
 });
 
+app.get('/qrcode1', function(req, res) {
+  var code = qr.svgObject('45304c60-9eac-48bf-9d0b-c02dda6c6cb3', { type: 'svg', size: 5 });
+  //console.log(code);
+  res.render('index', {path: code.path});
+});
+
+app.get('/qrcode2', function(req, res) {
+  var code = qr.svgObject('8f14886c-d267-44b8-8518-8cf363634929', { type: 'svg', size: 5 });
+  //console.log(code);
+  res.render('index', {path: code.path});
+});
 
 app.listen(port, function (err) {
     console.log('[%s] Listening on http://localhost:%d', app.settings.env, port);
