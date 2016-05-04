b7a710c586b5358f468fd0dea4087812b03c7688
Khanh Dao <kdao@Khanhs-MBP.attlocal.net>
2016-05-03 21:10:23 -0700

    Initial check in

--- geo-coder/test.js
+++ geo-coder/test.js
@@ -7,32 +7,15 @@ var extra = {};
 var geocoder = require('node-geocoder')(geocoderProvider, httpAdapter, extra);
 
 // Using callback
-geocoder.geocode('29 champs elysée paris', function(err, res) {
+geocoder.geocode('4900 Marie P DeBartolo Way, Santa Clara, CA 95054', function(err, res) {
     console.log(res);
 });
 
 // Or using Promise
-geocoder.geocode('29 champs elysée paris')
+/*geocoder.geocode('29 champs elysée paris')
     .then(function(res) {
         console.log(res);
     })
     .catch(function(err) {
         console.log(err);
-    });
-
-// output :
-[{
-    latitude: 48.8698679,
-    longitude: 2.3072976,
-    country: 'France',
-    countryCode: 'FR',
-    city: 'Paris',
-    zipcode: '75008',
-    streetName: 'Champs-Élysées',
-    streetNumber: '29',
-    administrativeLevels:
-     { level1long: 'Île-de-France',
-       level1short: 'IDF',
-       level2long: 'Paris',
-       level2short: '75' }
-}]
+    });*/
